2dab99305154d07228e80cd154e25a28e185da48
Artem Novichkov <artem.novichkov@rosberry.com>
2018-05-04 10:23:46 +0600

    Update install logic

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+NAME="11.4 (15F5061c).zip"
 cd /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport
-curl https://raw.githubusercontent.com/artemnovichkov/device-support/master/11.4%20(15F5061c).zip
+curl https://raw.githubusercontent.com/artemnovichkov/device-support/master/11.4%20\(15F5061c\).zip -o "${NAME}"
+unzip "${NAME}"
+rm "${NAME}"
