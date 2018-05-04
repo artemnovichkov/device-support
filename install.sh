b795ec7590d25a296471e3f9ee75ee2e93e6724a
Artem Novichkov <artem.novichkov@rosberry.com>
2018-05-04 09:36:09 +0600

    Update archive url

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport
-curl "https://github.com/artemnovichkov/device-support/master/11.4 (15F5061c).zip"
+curl "https://github.com/artemnovichkov/device-support/blob/master/11.4%20(15F5061c).zip"
